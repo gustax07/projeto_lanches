bbf7b766e4e722e1417ebaf4a8fe909b92a7b794
Gustavo <gfsvieira@gmail.com>
2025-12-02 16:03:32 -0300

    Initial commit

---
+++ classes/Banco.class.php
@@ -0,0 +1,39 @@
+<?php
+ 
+class Banco
+{
+    private static $dbNome = 'projeto_lanches';
+    private static $dbHost = '10.141.46.35';
+    private static $dbUsuario = 'admin';
+    private static $dbSenha = 'senac';
+    
+    private static $cont = null;
+    
+    public function __construct() 
+    {
+        die('A função Init nao é permitido!');
+    }
+    
+    public static function conectar()
+    {
+        if(null == self::$cont)
+        {
+            try
+            {
+                self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbNome, self::$dbUsuario, self::$dbSenha); 
+            }
+            catch(PDOException $exception)
+            {
+                die($exception->getMessage());
+            }
+        }
+        return self::$cont;
+    }
+    
+    public static function desconectar()
+    {
+        self::$cont = null;
+    }
+}
+ 
+?>
