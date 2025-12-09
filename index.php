2ba4d59e436900edc8daf053ad73650b4500b107
DouglasGomes0 <douglasgomesaqui@gmail.com>
2025-12-09 14:43:37 -0300

    adicionado iframes no index.php

--- index.php
+++ index.php
@@ -2,3 +2,40 @@
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lanches</title>
+    <style>
+        body {
+            margin: 0px;
+            border: none;
+            padding: 0px;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- iframe do header -->
+    <iframe src="./header.php"
+        style="width: 100%; height: 130px; margin: 0; border: none;">
+    </iframe>
+    <!-- iframe do nav -->
+    <iframe src="./nav.php"
+        style="width: 100%; height: 60px; margin: 0; border: none;">
+    </iframe>
+    <!-- iframe do corpo -->
+    <iframe src="./lanches.php"
+        style="width: 100%; height: 100vh; margin: 0; border: none; padding: 0;">
+    </iframe>
+    <!-- iframe do footer -->
+    <iframe src="./footer.html"
+        style="width: 100%; height: 510px; margin: 0; border: none; padding: 0;">
+    </iframe>
+</body>
+
+</html>
