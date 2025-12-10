0c12bc1c9313443906f7042ab42a9f5471cdb5a8
Pablo2301-dvl <par190833x@gmail.com>
2025-12-10 16:40:50 -0300

    Estilizando o header

---
+++ header.php
@@ -0,0 +1,19 @@
+<?php // header.php ?>
+<link rel="stylesheet" href="../projeto_lanches/css/header.css">
+<header class="site-header">
+  <div class="container">
+    <div class="header-content">
+      
+      <!-- Logo/Título centralizado -->
+      <h1 class="logo-title">
+        <a href="../projeto_lanches/header.php">Tasty Burguer</a>
+      </h1>
+
+      <!-- Botões direita -->
+      <div class="header-buttons">
+        <a href="../projeto_lanches/admin/logar.php" class="btn-login">Logar</a>
+        <a href="#" class="btn-register">Cadastre-se</a>
+      </div>
+    </div>
+  </div>
+</header>
