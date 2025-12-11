113aba8ea5936635555a1acf0cdccdd5a18750ef
DouglasGomes0 <douglasgomesaqui@gmail.com>
2025-12-11 17:08:07 -0300

    adição da linha 2 na página do cardápio, adição do iframe do footer na página do cardápio, estilização da barra de scroll

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="./images/icon_burguer.png">
-    <title>Lanches</title>
+    <title>Tasty Burguer - home</title>
     <style>
         body {
             margin: 0px;
@@ -18,25 +18,27 @@
             padding: 0px;
             overflow: hidden;
         }
+
     </style>
 </head>
 
 <body>
-    <div class="topo" style="position: fixed; width: 100%;">
-    <!-- iframe do header -->
-    <iframe src="./header.php"
-        style="width: 100%; height: 155px; margin: 0; border: none;">
-    </iframe>
-    <!-- iframe do nav -->
-    <iframe src="./nav.php"
-        style="width: 100%; height: 60px; margin: 0; border: none;">
-    </iframe>
+    <div class="topo" style="position: fixed; width: 100%; padding: 0px; ">
+        <!-- iframe do header -->
+        <iframe src="./header.php" frameborder="0" scrolling="no" id="headerIframe"
+            style="width: 100%; height: 155px; margin: 0; border: none; padding: 0 ; overflow: hidden;">
+        </iframe>
+        <!-- iframe do nav -->
+        <iframe src="./nav.php"
+            style="width: 100%; height: 72px; margin: 0; border: none; border-radius: 0 0 50% 50%; overflow: hidden;">
+        </iframe>
     </div>
     <!-- iframe do corpo -->
-    <iframe src="./lanches.php"
-        style="width: 100%; height: 100vh; margin: 0; border: none; padding: 0;">
+    <iframe src="./lanches.php" id="corpoIframe"
+        style="width: 100vw; height: 100vh; margin: 0; border: none; padding: 0;">
     </iframe>
-    
+
+
 </body>
 
 </html>
--- lanches.php
+++ lanches.php
@@ -18,12 +18,36 @@ header('Content-type: text/html; charset=utf-8');
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Hello, world!</title>
 </head>
+<style>
+    ::-webkit-scrollbar {
+        width: 2px;
+    }
+
+    ::-webkit-scrollbar-track {
+        background: transparent;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background: rgba(160, 160, 160, 0.9);
+        border-radius: 10px;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+        background: rgba(140, 140, 140, 0.6);
+    }
+
+    body {
+        overflow-x: hidden;
+    }
+</style>
 
 <body style="margin: 0px; border: none; padding: 0px;">
     <div class="container-fluid" style="margin: 0px; border: none; padding: 0px;">
+        <div style="background-color: black; height: 100px; width: 100%;"> </div>
+
 
         <div class="row"> <!-- Linha 1 || começo --> <!-- carrossel de imagens -->
-            <div class="col-12">
+            <div class="col-12" style="padding: 0px;">
                 <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
@@ -48,6 +72,12 @@ header('Content-type: text/html; charset=utf-8');
             </div>
         </div> <!-- Linha 1 || final -->
 
+        <div class="row"> <!-- Linha 2 || começo -->
+            <div class="col-12" style="text-align: center; margin: 10px">
+                <h1>Lanches</h1>
+            </div>
+        </div> <!-- Linha 2 || final -->
+
         <div class="row" style="gap: 30px; justify-content: space-evenly;"> <!-- Linha 3 || começo --> <!-- modelo dos cards -->
             <?php foreach ($itens_listar as $i) { ?>
                 <div class="col-2">
@@ -64,7 +94,9 @@ header('Content-type: text/html; charset=utf-8');
             <?php } ?>
         </div> <!-- Linha 3 || fim -->
     </div>
-
+    <iframe src="./footer.html"
+        style="height: 40vh; width: 100vw; margin: 0; border: none; padding: 0 ; overflow: hidden;">
+    </iframe>
     <!-- Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
