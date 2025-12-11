f8637de9508e3a9a70615cadc2febd07ee5e2be8
Pablo2301-dvl <par190833x@gmail.com>
2025-12-11 14:54:14 -0300

    Estilizando o nav

---
+++ nav.php
@@ -0,0 +1,11 @@
+<nav class="nav-minimal">
+    <link rel="stylesheet" href="./css/nav.css">
+
+    <ul>
+        <li><a href="#">Início</a></li>
+        <li><a href="#" class="active">Lanches</a></li>
+        <li><a href="#">Promoções</a></li>
+        <li><a href="#">Bebidas</a></li>
+        <li><a href="#">Contato</a></li>
+    </ul>
+</nav>
