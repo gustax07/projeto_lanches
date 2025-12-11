d2fdd4ffc73a5db7d5b6ebccd4dc149e6e007081
Gustavo <gfsvieira@gmail.com>
2025-12-11 17:09:24 -0300

    começando a mexer na tela painel dos admistradores

--- admin/painel.php
+++ admin/painel.php
@@ -15,33 +15,44 @@ if (!isset($_SESSION['usuario'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Painel Administrador</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    </style>
 </head>
 
-<body>
-    <div class="card">
-        <div class="card-body">
-            <div class="row align-items-center justify-content-center">
-                <div class="col-auto">
-                    <h1>Painel Administrador</h1>
+<body style="margin:0; border: none; padding: 0;">
+    <div class="row">
+        <div class="col-auto">
+            <div class="card">
+                <div class="card-body vh-100 bg-dark">
+                    <div class="vstack gap-3" style="width: 100%;">
+                        <a href="lanches.php" class="btn btn-dark btn-lg">Lanches</a>
+                        <a href="tipos.php" class="btn btn-dark btn-lg">Tipos</a>
+                        <a href="categorias.php" class="btn btn-dark btn-lg">Categorias</a>
+                        <a href="funcionarios.php" class="btn btn-dark btn-lg">Funcionarios</a>
+                    </div>
                 </div>
-                <div class="col-auto d-flex justify-content-end">
-                    <a class="btn btn-danger" href="../actions/funcionarios/sair_funcionarios.php">Sair</a>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-auto">
+                            <h5 class="card-title">Painel Administrador</h5>
+                            <p class="card-text">Bem vindo ao painel administrador</p>
+                        </div>
+                        
+                            <div class="col-auto">
+                                <a href="gerenciar_funcionarios.php" class="btn btn-danger">Sair</a>
+                            </div>
+                        
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="text-center">
-        <h2>Ola, Administrador</h2>
-    </div>
-    <div class="">
-    <h4>Painel de Controle</h4>
-    <a class="btn btn-dark" href="./gerenciar_funcionarios.php?id_tipo_fk=-1">Gerenciar Funcionarios</a>
-    <a class="btn btn-dark" href="./gerenciar_lanches.php">Gerenciar Lanches</a>
-    <a class="btn btn-dark" href="./gerenciar_pedidos.php">Gerenciar Pedidos</a>
+    <?php include_once("../includes/bootstrap_include.php"); ?>
 
-    <?php
-    include_once("../includes/bootstrap_include.php")
-    ?>
 </body>
 
 </html>
