9a40f575ba42727675c3b0df385d0d5998a86acb
gustax07 <gfsvieira@gmail.com>
2026-02-19 12:23:00 -0300

    feito um header & nav para area administrativa

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -13,30 +13,61 @@
 
     <title>Painel Administrador</title>
     <style>
-        iframe {
-            display: block;
+
+        a {
+            text-decoration: none;
+            color: black;
+            font-weight: bold;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            transition: all 0.3s ease-in-out !important;
+            font-size: 18px !important;
+        }
+
+        a:hover {
+            font-weight: bold;
+            transform: translateY(-5px);
+            cursor: pointer;
+        }
+
+        navbar-brand {
+            font-weight: bold;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        }
+
+        .tasty {
+            font-size: 48px;
         }
     </style>
 </head>
 
 <body>
-    <nav class="navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar-expand-lg bg-text-light bg-body-tertiary shadow-lg py-3">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
-                <img src="../images/icon_burguer.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-                Tasty Burger
+                <div class="tasty">
+                    <img src="../images/icon_burguer.png" alt="Logo" width="65" height="60" class="d-inline-block align-text-top">
+                    Tasty Burger
+                </div>
             </a>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <a class="nav-link active" aria-current="page" href="index.php"> Pagina Inicial</a>
-            <a class="nav-link active" aria-current="page" href="pedidos.php">Pedidos</a>
+            <div class="row align-items-center">
+                <div class="col d-flex gap-3 mt-2 flex-wrap nowrap">
+                    <a class="nav-link" aria-current="page" href="index.php">Pagina Inicial</a>
+                    <a class="nav-link" aria-current="page" href="pedidos.php">Pedidos</a>
+                    <a class="nav-link" aria-current="page" href="gerenciar_funcionarios.php">Funcionarios</a>
+                    <a class="nav-link" aria-current="page" href="gerenciar_categorias.php">Categorias</a>
+                    <a class="nav-link" aria-current="page" href="gerenciar_produtos.php">Produtos</a>
+                </div>
+                <div class="col-auto" style="background-color: red;">
+                    <a class="nav-link" aria-current="page" href="gerenciar_funcionarios.php"> Sair</a>
+                </div>
             </div>
-
         </div>
+
     </nav>
-   
- 
+
+
     <?php include_once("../includes/bootstrap_include.php"); ?>
-        
+
 </body>
 
 </html>
