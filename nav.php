5936b2923e2971fc896bf7e6bfebebe5446071a8
DouglasGomes0 <douglasgomesaqui@gmail.com>
2026-03-02 14:59:00 -0300

    melhoria no nav - botão pra expandir e recolher o nav

--- nav.php
+++ nav.php
@@ -1,5 +1,7 @@
 <?php
 include('classes/categorias.class.php');
+include('includes/bootstrap_include.php');
+
 $categorias = new Categorias();
 $categorias_listar = $categorias->Listar();
 ?>
@@ -13,23 +15,35 @@ $categorias_listar = $categorias->Listar();
     <link rel="stylesheet" href="./css/nav.css">
 </head>
 
-<body>
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
     <div class="container-fluid">
 
         <a class="navbar-brand" href="index.php">🍔</a>
 
-        <ul class="navbar-nav me-auto mb-2 mb-md-0">
-            <?php foreach ($categorias_listar as $c) { ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php?id=<?= $c['id'] ?>">
-                        <?= $c['nome'] ?>
-                    </a>
-                </li>
-            <?php } ?>
-        </ul>
+        <button class="navbar-toggler" type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#menuResponsivo"
+            aria-controls="menuResponsivo"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="menuResponsivo">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+
+                <?php foreach ($categorias_listar as $c) { ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?id=<?= $c['id'] ?>">
+                            <?= $c['nome'] ?>
+                        </a>
+                    </li>
+                <?php } ?>
+
+            </ul>
+        </div>
 
     </div>
 </nav>
-</body>
+
 </html>
